bd6aa9dd3824dba21779c954b3299ae67a077a14
A NEETS <alex.neets@gmail.com>
2022-06-28 14:11:54 -0400

    Update info modal.

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -1,3 +1,7 @@
+import { Cell } from '../grid/Cell'
+import { CompletedRow } from '../grid/CompletedRow'
+import { CurrentRow } from '../grid/CurrentRow'
+import { EndRow } from '../grid/EndRow'
 import { BaseModal } from './BaseModal'
 import { MAX_EDIT_DISTANCE } from '../../constants/settings'
 
@@ -10,14 +14,30 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
+        {/* <p className="flex justify-left text-sm text-gray-500 dark:text-gray-300"> */}
         Get from the start word to the end word by entering intermediate words.
-        Each word should differ from the last by a{' '}
-        <a href="https://en.wikipedia.org/wiki/Levenshtein_distance">
-          Levenshtein distance
-        </a>{' '}
-        of at most {MAX_EDIT_DISTANCE}. The good news is you may use as many
-        words as you like to complete the path!
+        Each word should differ from the last by at most {MAX_EDIT_DISTANCE}{' '}
+        <a href="https://en.wikipedia.org/wiki/Hamming_distance">
+          letter substitutions
+        </a>
+        . The good news is you may use as many words as you like to complete the
+        path from the start word to the end word!
       </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <EndRow word="WEARY" />
+      </div>
+      <div>
+        <CompletedRow
+          key={0}
+          endWord="LEANS"
+          guess="LEAR_"
+          isRevealing={true}
+        />
+      </div>
+      <div className="flex justify-center mb-1 mt-4">
+        <EndRow word="LEANS" />
+      </div>
     </BaseModal>
   )
 }
