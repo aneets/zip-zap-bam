1d51b3966b786becf0824f287e68ddafd6c34a07
A NEETS <alex.neets@gmail.com>
2022-07-12 13:03:50 -0400

    Update text for testing.

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -26,9 +26,9 @@ export const SHARE_TEXT = 'Share'
 export const MIGRATE_BUTTON_TEXT = 'Transfer'
 export const MIGRATE_DESCRIPTION_TEXT =
   'Click here to transfer your statistics to a new device.'
-export const MIN_PATH_TEXT = 'Minimum intermediate words'
-export const TOTAL_TRIES_TEXT = 'Total tries'
-export const CURRENT_STREAK_TEXT = 'Current streak'
-export const BEST_STREAK_TEXT = 'Best streak'
+export const MIN_PATH_TEXT = 'Minimum intermediate words:'
+export const TOTAL_TRIES_TEXT = 'Total tries:'
+export const CURRENT_STREAK_TEXT = 'Current streak:'
+export const BEST_STREAK_TEXT = 'Best streak:'
 export const DISCOURAGE_INAPP_BROWSER_TEXT =
   "You are using an embedded browser and may experience problems sharing or saving your results. We encourage you rather to use your device's default browser."
